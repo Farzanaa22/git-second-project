e3d0e9200af9086e9cefea199f8039a930de0f37
Farzanaa22 <farzanaa.mosawi@gmail.com>
2023-08-14 17:44:49 -0700

    changes to be commited

---
+++ src/git/tek/SecondClassFromNewBranch.java
@@ -0,0 +1,10 @@
+package git.tek;
+
+public class SecondClassFromNewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the changes from my new branch");
+	}
+
+}
